b97e2876d7b7914b7fd97928d204d2e79b4cba2b
Daniel Cazzulino <daniel@cazzulino.com>
2022-05-26 03:41:26 -0300

    Add quote loading and routes

--- dolar/Program.cs
+++ dolar/Program.cs
@@ -1,14 +1,85 @@
-﻿var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+﻿using System.Globalization;
+using System.Net;
+using System.Text.Json.Serialization;
+using Devlooped.Xml.Css;
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient("http")
+    .ConfigurePrimaryHttpMessageHandler(() =>
+        new ContentLocationHttpHandler(
+            new XhtmlHttpHandler(new HttpClientHandler
+            {
+                AllowAutoRedirect = true,
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
+                UseDefaultCredentials = true,
+                UseProxy = false,
+                Proxy = null
+            })));
+
+var app = builder.Build();
 
 app.UseRouting();
 
-app.MapGet("/", () => "Hello World");
+app.MapGet("/", async (IHttpClientFactory http) =>
+{
+    var (status, quotes) = await GetQuotesAsync(http.CreateClient("http"));
+    if (status != HttpStatusCode.OK)
+        return Results.StatusCode((int)status);
+
+    return Results.Ok(quotes!);
+});
+
+app.MapGet("/{id}", async (string id, IHttpClientFactory http) =>
+{
+    var (status, quotes) = await GetQuotesAsync(http.CreateClient("http"));
+    if (status != HttpStatusCode.OK)
+        return Results.StatusCode((int)status);
+
+    if (!quotes!.ContainsKey(id))
+        return Results.NotFound();
+
+    return Results.Ok(quotes[id]);
+});
 
 app.Run();
+
+async Task<(HttpStatusCode, Dictionary<string, Quote>?)> GetQuotesAsync(HttpClient http)
+{
+    var response = await http.GetAsync("https://www.infobae.com/economia/");
+
+    if (!response.IsSuccessStatusCode)
+        return (response.StatusCode, default);
+
+    response.EnsureSuccessStatusCode();
+    var doc = await response.Content.ReadAsDocumentAsync();
+    var exchange = doc.CssSelectElement(".excbar");
+    if (exchange == null)
+        return (HttpStatusCode.NotFound, default);
+
+    var quotes = exchange.CssSelectElements("a")
+        .Select(x => new
+        {
+            title = x.CssSelectElement("p.exc-tit")?.Value.Replace("\"", ""),
+            value = double.Parse(x.CssSelectElement("p.exc-val")?.Value ?? "0", NumberStyles.AllowDecimalPoint),
+            url = x.CssSelectElement("p.exc-tit")?.Value switch
+            {
+                string s when s.Contains("Bco") => "/oficial",
+                string s when s.Contains("Libre") => "/blue",
+                string s when s.Contains("Solidario") => "/tarjeta",
+                string s when s.Contains("liqui") => "/ccl",
+                string s when s.Contains("MEP") => "/mep",
+                _ => "/"
+            }
+        })
+        .Where(x => x.url != "/" && x.title != null)
+        .Select(x => new Quote(x.url.Substring(1), x.title!, x.value))
+        .ToDictionary(x => x.Id);
+
+    return (HttpStatusCode.OK, quotes);
+}
+
+record Quote([property: JsonIgnore] string Id, string Title, double Price)
+{
+    public string Url = "/" + Id;
+}
