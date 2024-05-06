6c0d4c6e1f551d60d7d878995f4f20ec05e40b84
Daniel Cazzulino <daniel@cazzulino.com>
2024-05-06 20:11:59 -0300

    Switch to Azure Functions now that it supports .NET8 isolated

---
+++ dolar/Dolar.cs
@@ -0,0 +1,97 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Devlooped.Web;
+using System.Globalization;
+using System.Text.Json.Serialization;
+
+public class Dolar(ILogger<Dolar> logger, IHttpClientFactory http)
+{
+    [Function("api")]
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "{id:alpha?}")] HttpRequest req, string? id)
+    {
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        var (status, quotes) = await GetQuotesAsync(http.CreateClient("http"));
+        if (status != HttpStatusCode.OK)
+        {
+            logger.LogWarning("Could not fetch webpage from Infobae. Status: {status}", status);
+            return new StatusCodeResult((int)status);
+        }
+
+        if (!(quotes?.Count > 0))
+        {
+            logger.LogWarning("No quotes found in Infobae webpage.");
+            return new NotFoundResult();
+        }
+
+        id ??= req.Query.Keys.FirstOrDefault(x => x != "badge");
+        var quote = default(Quote);
+
+        if (id != null && !quotes.TryGetValue(id, out quote))
+            return new NotFoundObjectResult("Invalid dolar kind. Must be one of: oficial/blue/tarjeta/mep/ccl.");
+
+        if (req.Query.Keys.Contains("badge"))
+        {
+            // Can only return badge for a single value
+            if (id == null)
+                return new BadRequestObjectResult("Need to specify oficial/blue/tarjeta/mep/ccl.");
+
+            return new OkObjectResult(new
+            {
+                schemaVersion = 1,
+                label = quote!.Title,
+                message = "$ " + quote.Price
+            });
+        }
+        else if (id != null)
+        {
+            return new OkObjectResult(quote!.Price);
+        }
+
+        return new OkObjectResult(quotes);
+
+    }
+
+    async Task<(HttpStatusCode, Dictionary<string, Quote>?)> GetQuotesAsync(HttpClient http)
+    {
+        var response = await http.GetAsync("https://www.infobae.com/economia/");
+        if (!response.IsSuccessStatusCode)
+            return (response.StatusCode, default);
+
+        var doc = await response.Content.ReadAsDocumentAsync();
+        var exchange = doc.CssSelectElement(".exchange-dolar-container");
+        if (exchange == null)
+            return (HttpStatusCode.NotFound, default);
+
+        var quotes = exchange.CssSelectElements(".exchange-dolar-item")
+            .Select(x => new
+            {
+                title = x.CssSelectElement("a p")?.Value,
+                value = double.Parse(
+                    x.CssSelectElement(".exchange-dolar-amount")?.Value.Trim().TrimStart('$') ?? "0", 
+                    NumberStyles.AllowDecimalPoint | NumberStyles.AllowThousands, CultureInfo.GetCultureInfo("es-AR")),
+                url = x.CssSelectElement("a p")?.Value switch
+                {
+                    string s when s.Contains("BANCO", StringComparison.OrdinalIgnoreCase) => "/oficial",
+                    string s when s.Contains("LIBRE", StringComparison.OrdinalIgnoreCase) => "/blue",
+                    string s when s.Contains("TURISTA", StringComparison.OrdinalIgnoreCase) => "/tarjeta",
+                    string s when s.Contains("LIQUI", StringComparison.OrdinalIgnoreCase) => "/ccl",
+                    string s when s.Contains("MEP", StringComparison.OrdinalIgnoreCase) => "/mep",
+                    _ => "/"
+                }
+            })
+            .Where(x => x.url != "/" && x.title != null)
+            .Select(x => new Quote(x.url.Substring(1), x.title!, x.value))
+            .ToDictionary(x => x.Id);
+
+        return (HttpStatusCode.OK, quotes);
+    }
+
+    record Quote([property: JsonIgnore] string Id, string Title, double Price)
+    {
+        public string Url = "/" + Id;
+    }
+}
--- dolar/Program.cs
+++ dolar/Program.cs
@@ -1,116 +1,87 @@
-﻿using System.Globalization;
-using System.Net;
-using System.Text.Json.Serialization;
-using Devlooped.Web;
+﻿using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddHttpClient("http")
-    .ConfigurePrimaryHttpMessageHandler(() =>
-        new ContentLocationHttpHandler(
-            new XhtmlHttpHandler(new HttpClientHandler
-            {
-                AllowAutoRedirect = true,
-                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
-                UseDefaultCredentials = true,
-                UseProxy = false,
-                Proxy = null
-            })));
-
-var app = builder.Build();
-
-app.UseRouting();
-
-app.MapGet("/", async (IHttpClientFactory http, HttpContext context, ILogger<Program> logger) =>
-{
-    var (status, quotes) = await GetQuotesAsync(http.CreateClient("http"));
-    if (status != HttpStatusCode.OK)
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services =>
     {
-        logger.LogWarning("Could not fetch webpage from Infobae. Status: {status}", status);
-        return Results.StatusCode((int)status);
-    }
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+        services.AddHttpClient("http")
+            .ConfigurePrimaryHttpMessageHandler(() =>
+                new ContentLocationHttpHandler(
+                    new XhtmlHttpHandler(new HttpClientHandler
+                    {
+                        AllowAutoRedirect = true,
+                        AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
+                        UseDefaultCredentials = true,
+                        UseProxy = false,
+                        Proxy = null
+                    })));
 
-    if (!(quotes?.Count > 0))
-    {
-        logger.LogWarning("No quotes found in Infobae webpage.");
-        return Results.NotFound();
-    }
+    })
+    .Build();
 
-    var id = context.Request.Query.Keys.FirstOrDefault(x => x != "badge");
-    var quote = default(Quote);
+host.Run();
 
-    if (id != null && !quotes.TryGetValue(id, out quote))
-        return Results.NotFound("Invalid dolar kind. Must be one of: oficial/blue/tarjeta/mep/ccl.");
+//var app = builder.Build();
 
-    if (context.Request.Query.Keys.Contains("badge"))
-    {
-        // Can only return badge for a single value
-        if (id == null)
-            return Results.BadRequest("Need to specify oficial/blue/tarjeta/mep/ccl.");
+//app.UseRouting();
 
-        return Results.Ok(new
-        {
-            schemaVersion = 1,
-            label = quote!.Title,
-            message = "$ " + quote.Price
-        });
-    }
-    else if (id != null)
-    {
-        return Results.Ok(quote!.Price);
-    }
+//app.MapGet("/", async (IHttpClientFactory http, HttpContext context, ILogger<Program> logger) =>
+//{
+//    var (status, quotes) = await GetQuotesAsync(http.CreateClient("http"));
+//    if (status != HttpStatusCode.OK)
+//    {
+//        logger.LogWarning("Could not fetch webpage from Infobae. Status: {status}", status);
+//        return Results.StatusCode((int)status);
+//    }
 
-    return Results.Ok(quotes);
-});
+//    if (!(quotes?.Count > 0))
+//    {
+//        logger.LogWarning("No quotes found in Infobae webpage.");
+//        return Results.NotFound();
+//    }
 
-app.MapGet("/{id}", async (string id, IHttpClientFactory http) =>
-{
-    var (status, quotes) = await GetQuotesAsync(http.CreateClient("http"));
-    if (status != HttpStatusCode.OK)
-        return Results.StatusCode((int)status);
+//    var id = context.Request.Query.Keys.FirstOrDefault(x => x != "badge");
+//    var quote = default(Quote);
 
-    if (!quotes!.TryGetValue(id, out var quote))
-        return Results.NotFound("Invalid dolar kind. Must be one of: oficial/blue/tarjeta/mep/ccl.");
+//    if (id != null && !quotes.TryGetValue(id, out quote))
+//        return Results.NotFound("Invalid dolar kind. Must be one of: oficial/blue/tarjeta/mep/ccl.");
 
-    return Results.Ok(quote.Price);
-});
+//    if (context.Request.Query.Keys.Contains("badge"))
+//    {
+//        // Can only return badge for a single value
+//        if (id == null)
+//            return Results.BadRequest("Need to specify oficial/blue/tarjeta/mep/ccl.");
 
-app.Run();
+//        return Results.Ok(new
+//        {
+//            schemaVersion = 1,
+//            label = quote!.Title,
+//            message = "$ " + quote.Price
+//        });
+//    }
+//    else if (id != null)
+//    {
+//        return Results.Ok(quote!.Price);
+//    }
 
-async Task<(HttpStatusCode, Dictionary<string, Quote>?)> GetQuotesAsync(HttpClient http)
-{
-    var response = await http.GetAsync("https://www.infobae.com/economia/");
-    if (!response.IsSuccessStatusCode)
-        return (response.StatusCode, default);
+//    return Results.Ok(quotes);
+//});
 
-    var doc = await response.Content.ReadAsDocumentAsync();
-    var exchange = doc.CssSelectElement(".exchange-dolar-container");
-    if (exchange == null)
-        return (HttpStatusCode.NotFound, default);
+//app.MapGet("/{id}", async (string id, IHttpClientFactory http) =>
+//{
+//    var (status, quotes) = await GetQuotesAsync(http.CreateClient("http"));
+//    if (status != HttpStatusCode.OK)
+//        return Results.StatusCode((int)status);
 
-    var quotes = exchange.CssSelectElements(".exchange-dolar-item")
-        .Select(x => new
-        {
-            title = x.CssSelectElement("a p")?.Value,
-            value = double.Parse(x.CssSelectElement(".exchange-dolar-amount")?.Value.Trim().TrimStart('$') ?? "0", NumberStyles.AllowDecimalPoint | NumberStyles.AllowThousands, CultureInfo.GetCultureInfo("es-AR")),
-            url = x.CssSelectElement("a p")?.Value switch
-            {
-                string s when s.Contains("BANCO", StringComparison.OrdinalIgnoreCase) => "/oficial",
-                string s when s.Contains("LIBRE", StringComparison.OrdinalIgnoreCase) => "/blue",
-                string s when s.Contains("TURISTA", StringComparison.OrdinalIgnoreCase) => "/tarjeta",
-                string s when s.Contains("LIQUI", StringComparison.OrdinalIgnoreCase) => "/ccl",
-                string s when s.Contains("MEP", StringComparison.OrdinalIgnoreCase) => "/mep",
-                _ => "/"
-            }
-        })
-        .Where(x => x.url != "/" && x.title != null)
-        .Select(x => new Quote(x.url.Substring(1), x.title!, x.value))
-        .ToDictionary(x => x.Id);
+//    if (!quotes!.TryGetValue(id, out var quote))
+//        return Results.NotFound("Invalid dolar kind. Must be one of: oficial/blue/tarjeta/mep/ccl.");
 
-    return (HttpStatusCode.OK, quotes);
-}
+//    return Results.Ok(quote.Price);
+//});
 
-record Quote([property: JsonIgnore] string Id, string Title, double Price)
-{
-    public string Url = "/" + Id;
-}
+//app.Run();
